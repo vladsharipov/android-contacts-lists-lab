4cdc1e534e511cf6d3cb050ff6d92df9d13773d3
VFSharipov <sharipovvf@gmail.com>
2022-12-17 16:01:24 +0300

    Починил фильтр типов контактов

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.contacts.presentation.filter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -60,13 +61,16 @@ public class FilterContactTypeViewModel extends BaseBottomSheetViewModel {
     }
 
     private void updateFilterContactTypes() {
-        // создайте массив строк. Для инициализации используйте метод ContactType.getContactTypes(),
+        String[] filterContactTypes = ContactType.getContactTypes();// создайте массив строк. Для инициализации используйте метод ContactType.getContactTypes(),
         // который должен возвращать список всех доступных источников контактов
 
-        // создайте список типа FilterContactTypeUi и заполните его с помощью цикла forEach
-        // forEach должен бежать по строковому массиву, который вы создали ранее
+        ArrayList<FilterContactTypeUi> filterContactTypeUisList = new ArrayList<>();// создайте список типа FilterContactTypeUi и заполните его с помощью цикла forEach
+        filterContactTypeUisList.add(createAllSelectedItem(filterContactTypes));
+        for (String filterContactType : filterContactTypes) {
+            filterContactTypeUisList.add(createFilterContactType(filterContactType));
+        }// forEach должен бежать по строковому массиву, который вы создали ранее
 
-        // вызовите меотод setValue() у переменной filterContactTypesLiveDate и передайте в качестве аргументы ваш список типа FilterContactTypeUi
+        filterContactTypesLiveDate.setValue(filterContactTypeUisList); // вызовите меотод setValue() у переменной filterContactTypesLiveDate и передайте в качестве аргументы ваш список типа FilterContactTypeUi
     }
 
     @NonNull
